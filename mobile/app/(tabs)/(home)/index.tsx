57a24cc5ac8d14cc673f6e9da36656e02be46974
kochq <matukoch@live.com>
2025-01-18 16:35:52 -0300

    feat: Refresh on pull

--- mobile/app/(tabs)/(home)/index.tsx
+++ mobile/app/(tabs)/(home)/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, FlatList } from "react-native";
+import { StyleSheet, FlatList, RefreshControl } from "react-native";
 import TransactionCard from "./Transaction";
 import { Transaction } from "@/types";
 import { TotalCard } from "./TotalCard";
@@ -8,9 +8,16 @@ import { Buttons } from "./Buttons";
 
 export default function HomeScreen() {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = () => {
+        setRefreshing(true);
+        getTransactions();
+        setRefreshing(false);
+    };
 
     const getTransactions = async () => {
-        let url = "http://192.168.100.195:8080/api/transactions/details";
+        let url = "http://serkq.org:8080/api/transactions/details";
 
         const response = await fetch(url);
         const data = await response.json();
@@ -35,6 +42,14 @@ export default function HomeScreen() {
                 data={transactions}
                 renderItem={({ item }) => <TransactionCard t={item} />}
                 keyExtractor={(item) => item.id.toString()}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                        colors={["grey"]}
+                        progressBackgroundColor={"black"}
+                    />
+                }
             />
         </Screen>
     );
