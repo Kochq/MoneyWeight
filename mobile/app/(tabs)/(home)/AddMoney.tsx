d64e4cbe67865c8478da47ffdb5ca58ce614b13a
kochq <matukoch@live.com>
2025-01-19 00:44:51 -0300

    feat: Deleting transactions and selecting date

--- mobile/app/(tabs)/(home)/AddMoney.tsx
+++ mobile/app/(tabs)/(home)/AddMoney.tsx
@@ -1,9 +1,18 @@
-import { View, TextInput, Button } from "react-native";
+import {
+    View,
+    TextInput,
+    Button,
+    Text,
+    Pressable,
+    StyleSheet,
+} from "react-native";
 import Screen from "./Screen";
 import { useEffect, useState } from "react";
 import { Picker } from "@react-native-picker/picker";
 import { Category } from "@/types";
+import { Ionicons } from "@expo/vector-icons";
 import { useTheme } from "../../../theme/ThemeContext";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 
 export default function AddMoney() {
     const { colors } = useTheme();
@@ -11,6 +20,57 @@ export default function AddMoney() {
     const [money, setMoney] = useState("");
     const [categories, setCategories] = useState<Category[]>([]);
     const [selectedCategory, setSelectedCategory] = useState<Category>();
+    const [date, setDate] = useState<Date>(new Date());
+
+    const styles = StyleSheet.create({
+        container: {
+            justifyContent: "center",
+            alignItems: "stretch",
+            width: "100%",
+            borderRadius: 10,
+            borderWidth: 1,
+            padding: 10,
+            borderColor: colors.textPrimary,
+            backgroundColor: colors.surface,
+            boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.1)",
+        },
+
+        dateBtn: {
+            backgroundColor: colors.primary,
+            alignItems: "center",
+            padding: 5,
+            borderRadius: 5,
+            margin: 5,
+        },
+
+        date: {
+            flexDirection: "row",
+            alignItems: "center",
+            width: "100%",
+            justifyContent: "center",
+        },
+    });
+
+    const onChange = (event: any, selectedDate: Date) => {
+        const currentDate = selectedDate;
+        setDate(currentDate);
+    };
+    const showMode = (currentMode: any) => {
+        DateTimePickerAndroid.open({
+            value: date,
+            onChange,
+            mode: currentMode,
+            is24Hour: true,
+        });
+    };
+
+    const showDatepicker = () => {
+        showMode("date");
+    };
+
+    const showTimepicker = () => {
+        showMode("time");
+    };
 
     const addTransaction = async () => {
         let url = "http://serkq.org:8080/api/transactions";
@@ -18,6 +78,10 @@ export default function AddMoney() {
         console.log("Name: " + name);
         console.log("Money: " + money);
         console.log("Category: " + selectedCategory);
+
+        // formatDate: "2023-12-27 20:18:43",
+        const formatDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
         try {
             const response = await fetch(url, {
                 method: "POST",
@@ -28,6 +92,7 @@ export default function AddMoney() {
                     subcategory_id: 1,
                     currency: "ARS",
                     payment_method: "cosa",
+                    date: formatDate,
                     exchange_rate: 1500,
                     notes: "esto es una nota",
                     from_account_id: 1,
@@ -38,6 +103,7 @@ export default function AddMoney() {
 
             console.log("Transaction added!");
             console.log(data);
+            alert(data.status);
         } catch (e) {
             console.error(e);
         }
@@ -61,11 +127,7 @@ export default function AddMoney() {
 
     return (
         <Screen>
-            <View
-                style={{
-                    width: "100%",
-                }}
-            >
+            <View style={styles.container}>
                 <TextInput
                     placeholder="Name"
                     value={name}
@@ -92,7 +154,26 @@ export default function AddMoney() {
                         />
                     ))}
                 </Picker>
-                <Button title="Add" onPress={addTransaction} />
+                <View style={styles.date}>
+                    <Text>Date: {date.toLocaleString()}</Text>
+                    <Pressable style={styles.dateBtn} onPress={showDatepicker}>
+                        <Ionicons
+                            name="calendar"
+                            size={24}
+                            color={colors.surface}
+                        />
+                    </Pressable>
+                    <Pressable style={styles.dateBtn} onPress={showTimepicker}>
+                        <Ionicons
+                            name="time"
+                            size={24}
+                            color={colors.surface}
+                        />
+                    </Pressable>
+                </View>
+                <Pressable style={styles.dateBtn} onPress={addTransaction}>
+                    <Text style={{ color: colors.surface }}>Add</Text>
+                </Pressable>
             </View>
         </Screen>
     );
--- mobile/app/(tabs)/(home)/transaction/[id].tsx
+++ mobile/app/(tabs)/(home)/transaction/[id].tsx
@@ -1,12 +1,35 @@
 import { useLocalSearchParams } from "expo-router";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
 export default function TransactionScreen() {
     const { id } = useLocalSearchParams();
 
+    const removeTransaction = async (id: any) => {
+        let url = `http://serkq.org:8080/api/transactions/${id}`;
+
+        // formatDate: "2023-12-27 20:18:43",
+        try {
+            const response = await fetch(url, { method: "DELETE" });
+
+            const data = await response.json();
+
+            console.log("Transaction deleted!");
+            alert(data.status);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
     return (
         <View style={styles.container}>
             <Text>Details of transaction {id} </Text>
+            <Pressable
+                onPress={() => {
+                    removeTransaction(id);
+                }}
+            >
+                <Text>Delete transaction</Text>
+            </Pressable>
         </View>
     );
 }
